e641190d038390f68006fa4c426c39bf0155f525
Mats Sommervold <mats.sommervold@gmail.com>
2019-09-10 13:26:52 +0200

    building to docs

--- docs/assets/scripts/App-f520e06767.js
+++ docs/assets/scripts/App-f520e06767.js
@@ -90,10 +90,23 @@
 /*!***********************************!*\
   !*** ./app/assets/scripts/App.js ***!
   \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("\n\n//# sourceURL=webpack:///./app/assets/scripts/App.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_Example_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/Example.js */ \"./app/assets/scripts/modules/Example.js\");\n\nvar ex = new _modules_Example_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\nex.method();\n\n//# sourceURL=webpack:///./app/assets/scripts/App.js?");
+
+/***/ }),
+
+/***/ "./app/assets/scripts/modules/Example.js":
+/*!***********************************************!*\
+  !*** ./app/assets/scripts/modules/Example.js ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nclass Example {\n  constructor(params) {\n    this.params = params;\n  }\n\n  method() {\n    console.log(\"example\");\n  }\n\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Example);\n\n//# sourceURL=webpack:///./app/assets/scripts/modules/Example.js?");
 
 /***/ })
 
